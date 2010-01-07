8494af20b27d652cc0587aacaea9a82fbb561625
edgar <edgar@spooky.local>
2010-01-06 23:15:37 -0200

    Add log in IndexAction and change log4j.xml

--- src/main/java/com/latorre/countfp/action/IndexAction.java
+++ src/main/java/com/latorre/countfp/action/IndexAction.java
@@ -1,11 +1,16 @@
 package com.latorre.countfp.action;
 
+import org.apache.log4j.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class IndexAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 	
+	private static final Logger log = Logger.getLogger(IndexAction.class);
+	
 	public String execute() {
+		log.info("Execute in IndexAction");
 		return SUCCESS;
 	}
 }
