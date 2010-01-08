da838ad9e6f53875570a5e575bb78407281de52e
latorre <edgar.latorrer@gmail.com>
2010-01-08 18:39:51 -0200

    Add lists complexities and types.

--- src/main/java/com/latorre/countfp/action/DataAction.java
+++ src/main/java/com/latorre/countfp/action/DataAction.java
@@ -1,5 +1,6 @@
 package com.latorre.countfp.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.latorre.countfp.model.Data;
@@ -7,8 +8,10 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class DataAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
+	private List<String> types;
+	private List<String> complexities;
 	private Data data;
-	private List<Data> datas;
+	private List<Data> datas = new ArrayList<Data>();
 	
 	public String execute() {
 		return SUCCESS;
@@ -18,7 +21,7 @@ public class DataAction extends ActionSupport {
 		if(data != null) {
 			datas.add(data);
 		}
-		return " ";
+		return SUCCESS;
 	}
 
 	public void setData(Data data) {
@@ -31,5 +34,21 @@ public class DataAction extends ActionSupport {
 
 	public List<Data> getDatas() {
 		return datas;
+	}
+
+	public void setTypes(List<String> types) {
+		this.types = types;
+	}
+
+	public List<String> getTypes() {
+		return types;
+	}
+
+	public void setComplexities(List<String> complexities) {
+		this.complexities = complexities;
+	}
+
+	public List<String> getComplexities() {
+		return complexities;
 	}
 }
